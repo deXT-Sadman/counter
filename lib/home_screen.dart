e19315712966fe85b3e202b64ee4119302c83274
Sadman <2258cse00945@manarat.ac.bd>
2026-06-12 10:11:09 +0600

    Implement HomeScreen with counter functionality and navigation to DetailsScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,1 +1,118 @@
-class HomeScreen {}
+import 'package:flutter/material.dart';
+import 'details_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  int counter = 0;
+  int currentIndex = 0;
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: "Home"),
+            Tab(text: "About"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Counter Value", style: TextStyle(fontSize: 20)),
+                const SizedBox(height: 10),
+                Text(
+                  "$counter",
+                  style: const TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: incrementCounter,
+                      child: const Text("Increment"),
+                    ),
+
+                    const SizedBox(width: 10),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) =>
+                                DetailsScreen(counterValue: counter),
+                          ),
+                        );
+                      },
+                      child: const Text("Open Details"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Center(
+            child: Text(
+              "Welcome",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            _tabController.animateTo(index);
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+        ],
+      ),
+    );
+  }
+}
